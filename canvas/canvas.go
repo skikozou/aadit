357dfe81e1a0a31b6904ec450c9a2fea95ba8800
skikozou <skikozou1596@outlook.jp>
2025-12-17 02:27:16 +0900

    update canvas ctrl

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,5 +1,7 @@
 package canvas
 
+import "os"
+
 type Canvas struct {
 	Width  int
 	Height int
@@ -51,4 +53,171 @@ func (c *Canvas) Fill(text string) {
 			c.Data[i][j] = rune(text[(i*c.Width+j)%(len(text))])
 		}
 	}
+}
+
+func (c *Canvas)DrawRect(x, y, w, h int, char rune) {
+	for dy := 0; dy < h; dy++ {
+		for dx := 0; dx < w; dx++ {
+			nx, ny := x+dx, y+dy
+			if nx >= 0 && nx < c.Width && ny >= 0 && ny < c.Height {
+				c.Data[ny][nx] = char
+			}
+		}
+	}
+}
+
+func (c *Canvas)DrawLine(x1, y1, x2, y2 int, char rune) {
+	dx := abs(x2 - x1)
+	dy := abs(y2 - y1)
+	sx := sign(x2 - x1)
+	sy := sign(y2 - y1)
+	err := dx - dy
+	
+	x, y := x1, y1
+	
+	for {
+		if x >= 0 && x < c.Width && y >= 0 && y < c.Height {
+			c.Data[y][x] = char
+		}
+		
+		if x == x2 && y == y2 {
+			break
+		}
+		
+		e2 := 2 * err
+		if e2 > -dy {
+			err -= dy
+			x += sx
+		}
+		if e2 < dx {
+			err += dx
+			y += sy
+		}
+	}
+}
+
+func (c *Canvas)DrawBox(x, y, w, h int) {
+	if w < 2 || h < 2 {
+		return
+	}
+	
+	// 上下
+	for dx := 0; dx < w; dx++ {
+		nx := x + dx
+		if nx >= 0 && nx < c.Width {
+			if y >= 0 && y < c.Height {
+				if dx == 0 {
+					c.Data[y][nx] = '┌'
+				} else if dx == w-1 {
+					c.Data[y][nx] = '┐'
+				} else {
+					c.Data[y][nx] = '─'
+				}
+			}
+			if y+h-1 >= 0 && y+h-1 < c.Height {
+				if dx == 0 {
+					c.Data[y+h-1][nx] = '└'
+				} else if dx == w-1 {
+					c.Data[y+h-1][nx] = '┘'
+				} else {
+					c.Data[y+h-1][nx] = '─'
+				}
+			}
+		}
+	}
+	
+	// 左右
+	for dy := 1; dy < h-1; dy++ {
+		ny := y + dy
+		if ny >= 0 && ny < c.Height {
+			if x >= 0 && x < c.Width {
+				c.Data[ny][x] = '│'
+			}
+			if x+w-1 >= 0 && x+w-1 < c.Width {
+				c.Data[ny][x+w-1] = '│'
+			}
+		}
+	}
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func sign(n int) int {
+	if n < 0 {
+		return -1
+	}
+	if n > 0 {
+		return 1
+	}
+	return 0
+}
+
+func (c *Canvas)Save(filepath string) error {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	
+	for _, row := range c.Data {
+		for _, ch := range row {
+			if _, err := file.WriteString(string(ch)); err != nil {
+				return err
+			}
+		}
+		if _, err := file.WriteString("\n"); err != nil {
+			return err
+		}
+	}
+	
+	return nil
+}
+
+func (c *Canvas)Load(filepath string) error {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+	
+	lines := [][]rune{}
+	currentLine := []rune{}
+	
+	for _, ch := range string(data) {
+		if ch == '\n' {
+			lines = append(lines, currentLine)
+			currentLine = []rune{}
+		} else {
+			currentLine = append(currentLine, ch)
+		}
+	}
+	
+	// 最後の行が改行で終わっていない場合
+	if len(currentLine) > 0 {
+		lines = append(lines, currentLine)
+	}
+	
+	// キャンバスに適用
+	for i := 0; i < c.Height && i < len(lines); i++ {
+		for j := 0; j < c.Width && j < len(lines[i]); j++ {
+			c.Data[i][j] = lines[i][j]
+		}
+		// 行が短い場合は空白で埋める
+		for j := len(lines[i]); j < c.Width; j++ {
+			c.Data[i][j] = ' '
+		}
+	}
+	
+	// ファイルの行数がキャンバスより少ない場合は空白で埋める
+	for i := len(lines); i < c.Height; i++ {
+		for j := 0; j < c.Width; j++ {
+			c.Data[i][j] = ' '
+		}
+	}
+	
+	return nil
 }
--- custom.go
+++ custom.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 	"aadit/canvas"
 	"aadit/command"
@@ -20,7 +19,7 @@ func Customize(s tcell.Screen, cv *canvas.Canvas, con *command.Console, pop *pop
 				return "Usage: save <filename>"
 			}
 			
-			err := saveCanvas(cv, args[1])
+			err := cv.Save(args[1])
 			if err != nil {
 				return fmt.Sprintf("Error: %v", err)
 			}
@@ -33,7 +32,7 @@ func Customize(s tcell.Screen, cv *canvas.Canvas, con *command.Console, pop *pop
 				return "Usage: load <filename>"
 			}
 			
-			err := loadCanvas(cv, args[1])
+			err := cv.Load(args[1])
 			if err != nil {
 				return fmt.Sprintf("Error: %v", err)
 			}
@@ -88,7 +87,7 @@ func Customize(s tcell.Screen, cv *canvas.Canvas, con *command.Console, pop *pop
 			}
 			
 			char := rune(args[5][0])
-			drawRect(cv, x, y, w, h, char)
+			cv.DrawRect(x, y, w, h, char)
 			
 			return fmt.Sprintf("Rectangle at (%d,%d) %dx%d", x, y, w, h)
 		},
@@ -109,7 +108,7 @@ func Customize(s tcell.Screen, cv *canvas.Canvas, con *command.Console, pop *pop
 			}
 			
 			char := rune(args[5][0])
-			drawLine(cv, x1, y1, x2, y2, char)
+			cv.DrawLine(x1, y1, x2, y2, char)
 			
 			return fmt.Sprintf("Line from (%d,%d) to (%d,%d)", x1, y1, x2, y2)
 		},
@@ -129,176 +128,9 @@ func Customize(s tcell.Screen, cv *canvas.Canvas, con *command.Console, pop *pop
 				return "Error: invalid number format"
 			}
 			
-			drawBox(cv, x, y, w, h)
+			cv.DrawBox(x, y, w, h)
 			
 			return fmt.Sprintf("Box at (%d,%d) %dx%d", x, y, w, h)
 		},
 	}
-}
-
-func saveCanvas(cv *canvas.Canvas, filepath string) error {
-	file, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	
-	for _, row := range cv.Data {
-		for _, ch := range row {
-			if _, err := file.WriteString(string(ch)); err != nil {
-				return err
-			}
-		}
-		if _, err := file.WriteString("\n"); err != nil {
-			return err
-		}
-	}
-	
-	return nil
-}
-
-func loadCanvas(cv *canvas.Canvas, filepath string) error {
-	data, err := os.ReadFile(filepath)
-	if err != nil {
-		return err
-	}
-	
-	lines := [][]rune{}
-	currentLine := []rune{}
-	
-	for _, ch := range string(data) {
-		if ch == '\n' {
-			lines = append(lines, currentLine)
-			currentLine = []rune{}
-		} else {
-			currentLine = append(currentLine, ch)
-		}
-	}
-	
-	// 最後の行が改行で終わっていない場合
-	if len(currentLine) > 0 {
-		lines = append(lines, currentLine)
-	}
-	
-	// キャンバスに適用
-	for i := 0; i < cv.Height && i < len(lines); i++ {
-		for j := 0; j < cv.Width && j < len(lines[i]); j++ {
-			cv.Data[i][j] = lines[i][j]
-		}
-		// 行が短い場合は空白で埋める
-		for j := len(lines[i]); j < cv.Width; j++ {
-			cv.Data[i][j] = ' '
-		}
-	}
-	
-	// ファイルの行数がキャンバスより少ない場合は空白で埋める
-	for i := len(lines); i < cv.Height; i++ {
-		for j := 0; j < cv.Width; j++ {
-			cv.Data[i][j] = ' '
-		}
-	}
-	
-	return nil
-}
-
-func drawRect(cv *canvas.Canvas, x, y, w, h int, char rune) {
-	for dy := 0; dy < h; dy++ {
-		for dx := 0; dx < w; dx++ {
-			nx, ny := x+dx, y+dy
-			if nx >= 0 && nx < cv.Width && ny >= 0 && ny < cv.Height {
-				cv.Data[ny][nx] = char
-			}
-		}
-	}
-}
-
-func drawLine(cv *canvas.Canvas, x1, y1, x2, y2 int, char rune) {
-	dx := abs(x2 - x1)
-	dy := abs(y2 - y1)
-	sx := sign(x2 - x1)
-	sy := sign(y2 - y1)
-	err := dx - dy
-	
-	x, y := x1, y1
-	
-	for {
-		if x >= 0 && x < cv.Width && y >= 0 && y < cv.Height {
-			cv.Data[y][x] = char
-		}
-		
-		if x == x2 && y == y2 {
-			break
-		}
-		
-		e2 := 2 * err
-		if e2 > -dy {
-			err -= dy
-			x += sx
-		}
-		if e2 < dx {
-			err += dx
-			y += sy
-		}
-	}
-}
-
-func drawBox(cv *canvas.Canvas, x, y, w, h int) {
-	if w < 2 || h < 2 {
-		return
-	}
-	
-	// 上下
-	for dx := 0; dx < w; dx++ {
-		nx := x + dx
-		if nx >= 0 && nx < cv.Width {
-			if y >= 0 && y < cv.Height {
-				if dx == 0 {
-					cv.Data[y][nx] = '┌'
-				} else if dx == w-1 {
-					cv.Data[y][nx] = '┐'
-				} else {
-					cv.Data[y][nx] = '─'
-				}
-			}
-			if y+h-1 >= 0 && y+h-1 < cv.Height {
-				if dx == 0 {
-					cv.Data[y+h-1][nx] = '└'
-				} else if dx == w-1 {
-					cv.Data[y+h-1][nx] = '┘'
-				} else {
-					cv.Data[y+h-1][nx] = '─'
-				}
-			}
-		}
-	}
-	
-	// 左右
-	for dy := 1; dy < h-1; dy++ {
-		ny := y + dy
-		if ny >= 0 && ny < cv.Height {
-			if x >= 0 && x < cv.Width {
-				cv.Data[ny][x] = '│'
-			}
-			if x+w-1 >= 0 && x+w-1 < cv.Width {
-				cv.Data[ny][x+w-1] = '│'
-			}
-		}
-	}
-}
-
-func abs(n int) int {
-	if n < 0 {
-		return -n
-	}
-	return n
-}
-
-func sign(n int) int {
-	if n < 0 {
-		return -1
-	}
-	if n > 0 {
-		return 1
-	}
-	return 0
 }
